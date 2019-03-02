3f993d61c2b685c69de8530695f94334ff025d8b
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 03:49:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,5 +23,6 @@ def assign_rooms(array)
 end
 
 def printer(array)
-  batch_badge_creator(name).each {|badge| puts badge} 
+  batch_badge_creator(array).each {|badge| puts badge}
+  assign_rooms(array).each {|room| puts room}
 end
