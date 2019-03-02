3b9ab07bcff09a251dbf5e61086795471740fb7f
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 03:41:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def conference_badges
+
+end
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(array)
+  new_array = []
+  array.each_with_index do |name, index|
+    new_array.push ("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+  return new_array
+end
+
+def printer
+
+end
